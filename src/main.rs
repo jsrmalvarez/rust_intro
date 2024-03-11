6ffe093789ba4431ff63d13454f5757ff8b18494
aljs <joseramon.alvarez@betterask.erni>
2024-03-11 13:51:28 +0100

    This is full of errors , ready to be corrected

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
-struct User<'a> {
-	name: &'a String, // Struct with a must specify lifetimes
+// To be used as a key in a HashMap, must implement Hash, PartialEq, Eq
+struct User {
+	name: &String, // Struct with a must specify lifetimes
 }
 
 fn main() {
 	let user_1_name = String::from("Meg");
 	let user_2_name = String::from("Eve(Admin)");
-	let user_3_name = user_1_name.clone();
+	let user_3_name = user_1_name;
 
     let user_1 = User { name: &user_1_name }; // Cannot borrow after transferring ownership
     let user_2 = User { name: &user_2_name };
 	let user_3 = User { name: &user_3_name };
 
 
-	let mut database = HashMap::<&String, &[u32]>::default() ;  // Initialization required
+	let database:HashMap::<&String, &[u32]>;  // Initialization required
 	database.insert(user_1.name, &[1,2,3,4,5]); // Immutable by default
 	database.insert(user_2.name, &[42,43,44,45,46,47,48]);
 
@@ -25,7 +25,7 @@ fn main() {
 				println!("Printing {}'s data", user_1.name);
 				print_user_data_safe_implementation(user_data, 20)
 		}, // Containers are bound-checked
-		None => panic!("User not found"), // Patterns must be exhaustively covered!
+		//None => panic!("User not found"), // Patterns must be exhaustively covered!
 	}
 
 	println!("{}", user_3.name);
@@ -35,14 +35,6 @@ fn main() {
 	match database.try_reserve(500) {
 		Ok(()) => (),
 		Err(_) => panic!("Cannot reserve additional database records"),
-	}
-
-	// Dirty things must be inside an unsafe block
-	let x = 42;
-	unsafe {
-		// Example: dereferencing a raw pointer
-		let raw_ptr = &x as *const i32;
-		println!("Unsafe dereference: {}", *(raw_ptr.add(1)));
 	}
 
 }
@@ -57,10 +49,9 @@ fn print_user_data_safe_implementation(user_data:&[u32], len:usize){
 fn print_user_data_unsafe_implementation(user_data:&[u32], len:usize){
 
     for n in 0..len {
-		unsafe{
-			let raw_ptr = (&user_data[0] as *const u32).add(n);
-			println!("{:?}", *raw_ptr);
-		}
+		// Dirty things must be inside an unsafe block
+		let raw_ptr = (&user_data[0] as *const u32).add(n);
+		println!("{:?}", *raw_ptr);
     }
 }
 
@@ -72,6 +63,6 @@ mod tests{
 	#[should_panic]
 	fn test_heartbleed() {
 		let user_data = [76,32,43,54];
-		print_user_data_safe_implementation(&user_data, user_data.len() + 1);
+		print_user_data_unsafe_implementation(&user_data, user_data.len() + 1);
 	}
 }
