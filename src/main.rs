41033a5a16626b9080e9453bc4fb8b24d140c3a3
aljs <joseramon.alvarez@betterask.erni>
2024-03-11 13:38:25 +0100

    This compiles

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,21 @@
 use std::collections::HashMap;
 
-fn print_user_data_unsafe_implementation(user_data:&[u32], len:usize){
-
-    for n in 0..len {
-		unsafe{
-			let raw_ptr = (&user_data[0] as *const u32).add(n);
-			println!("{:?}", *raw_ptr);
-		}
-    }
-}
-
-fn print_user_data_safe_implementation(user_data:&[u32], len:usize){
-
-    for n in 0..len {
-        println!("{:?}", user_data[n]);
-    }
-}
-
-fn print_user_data(user_data:&[u32], len:usize){
-
-	print_user_data_safe_implementation(user_data, len);
-	//print_user_data_unsafe_implementation(user_data, len);
-}
-
-// Struct with a reference without lifetimes
 #[derive(Hash, PartialEq, Eq)]
 struct User<'a> {
-	name: &'a str,
+	name: &'a String, // Struct with a must specify lifetimes
 }
 
-
 fn main() {
-	let user_1_name  = String::from("Meg");
-	let user_2_name = "Eve(Admin)";
-	let user_3_name  = user_1_name.clone();
+	let user_1_name = String::from("Meg");
+	let user_2_name = String::from("Eve(Admin)");
+	let user_3_name = user_1_name.clone();
 
-    let user_1 = User { name: &user_1_name };
+    let user_1 = User { name: &user_1_name }; // Cannot borrow after transferring ownership
     let user_2 = User { name: &user_2_name };
 	let user_3 = User { name: &user_3_name };
 
 
-	let mut database = HashMap::<&str, &[u32]>::default() ;  // Initialization required
+	let mut database = HashMap::<&String, &[u32]>::default() ;  // Initialization required
 	database.insert(user_1.name, &[1,2,3,4,5]); // Immutable by default
 	database.insert(user_2.name, &[42,43,44,45,46,47,48]);
 
@@ -48,7 +23,7 @@ fn main() {
 	match database.get(user_1.name) {
 		Some(user_data) => {
 				println!("Printing {}'s data", user_1.name);
-				print_user_data(user_data, 20)
+				print_user_data_safe_implementation(user_data, 20)
 		}, // Containers are bound-checked
 		None => panic!("User not found"), // Patterns must be exhaustively covered!
 	}
@@ -72,4 +47,19 @@ fn main() {
 
 }
 
+fn print_user_data_safe_implementation(user_data:&[u32], len:usize){
 
+    for n in 0..len {
+        println!("{:?}", user_data[n]);
+    }
+}
+
+fn print_user_data_unsafe_implementation(user_data:&[u32], len:usize){
+
+    for n in 0..len {
+		unsafe{
+			let raw_ptr = (&user_data[0] as *const u32).add(n);
+			println!("{:?}", *raw_ptr);
+		}
+    }
+}
